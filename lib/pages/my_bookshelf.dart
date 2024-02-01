a9dbc6e740b6c0b9e7968eb7ec2dd48fc11344d4
jiu keem <jeewoo0521@naver.com>
2024-02-01 17:51:19 +0900

    feature; add skeleton pages

---
+++ lib/pages/my_bookshelf.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyBookshelf extends StatefulWidget {
+  const MyBookshelf({super.key});
+
+  @override
+  State<MyBookshelf> createState() => _MyBookshelfState();
+}
+
+class _MyBookshelfState extends State<MyBookshelf> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
