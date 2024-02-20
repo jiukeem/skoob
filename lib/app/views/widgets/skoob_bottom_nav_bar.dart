74b191a6a9710bd82c09d3204f58d046219c25fb
jiu keem <jeewoo0521@naver.com>
2024-02-20 18:47:10 +0900

    feature; implement bottom navigation bar based on figma design

---
+++ lib/app/views/widgets/skoob_bottom_nav_bar.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:skoob/app/utils/app_colors.dart';
+
+class SkoobBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  SkoobBottomNavBar({required this.currentIndex, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(28.0, 0, 28.0, 20.0),
+      height: 52.0,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+        border: Border.all(
+          color: AppColors.gray3,
+          width: 0.8
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.softBlack.withOpacity(0.1), // Shadow color
+            spreadRadius: 0,
+            blurRadius: 2, // Shadow blur radius
+            offset: Offset(0, 2), // Shadow position
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+        child: BottomAppBar(
+          surfaceTintColor: AppColors.white,
+          padding: EdgeInsets.zero,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              navBarItem(
+                  index: 0,
+                  defaultIcon: const Icon(
+                      FluentIcons.library_20_regular,
+                      color: AppColors.softBlack,
+                  ),
+                  selectedIcon: const Icon(
+                      FluentIcons.library_20_filled,
+                      color: AppColors.white,
+                  )
+              ),
+              navBarItem(
+                  index: 1,
+                  defaultIcon: const Icon(
+                      FluentIcons.book_search_20_regular,
+                      color: AppColors.softBlack,
+                  ),
+                  selectedIcon: const Icon(
+                      FluentIcons.book_search_20_filled,
+                      color: AppColors.white,
+                  )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget navBarItem({required int index, required Icon defaultIcon, required Icon selectedIcon}) {
+    bool isSelected = index == currentIndex;
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: Container(
+        height: 35.0,
+        width: 35.0,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: isSelected ? AppColors.primaryYellow : AppColors.white,
+        ),
+        child: isSelected ? selectedIcon : defaultIcon
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:skoob/app/controller/shared_list_state.dart';
-import 'package:skoob/skoob_frame.dart';
-import 'package:provider/provider.dart';
+import 'package:skoob/app/views/pages/bookshelf.dart';
+import 'package:skoob/app/views/pages/search.dart';
+import 'package:skoob/app/views/widgets/skoob_bottom_nav_bar.dart';
+import 'package:skoob/app/utils/app_colors.dart';
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(
@@ -10,3 +11,39 @@ void main() => runApp(MaterialApp(
   home: Skoob(),
 )
 );
+
+class Skoob extends StatefulWidget {
+  const Skoob({super.key});
+
+  @override
+  State<Skoob> createState() => _SkoobState();
+}
+
+class _SkoobState extends State<Skoob> {
+  int _currentPageIndex = 0;
+
+  final List<Widget> _pages = [
+    Search(),
+    Search(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentPageIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: SkoobBottomNavBar(
+        currentIndex: _currentPageIndex,
+        onTap: _onItemTapped
+      ),
+    );
+  }
+}
