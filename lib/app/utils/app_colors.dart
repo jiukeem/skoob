d04b9b5098ef03c50b75a3605aa519efb3a67614
jiu keem <jeewoo0521@naver.com>
2024-02-20 18:46:13 +0900

    resources; make appColors class

---
+++ lib/app/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryYellow = Color(0xFFFEB930);
+  static const Color primaryGreen = Color(0xFF184A3F);
+  static const Color secondaryYellow = Color(0xFFF8C85E);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color softBlack = Color(0xFF1E1E1E);
+  static const Color gray1 = Color(0xFF959595);
+  static const Color gray2 = Color(0xFFC9C9C9);
+  static const Color gray3 = Color(0xFFF2F2F2);
+  static const Color waringRed = Color(0xFFE10000);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:skoob/app/controller/shared_list_state.dart';
 import 'package:skoob/skoob_frame.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(
-    create: (context) => SharedListState(),
-    child: SkoobFrame(),
-  )
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: AppColors.primaryYellow, // Used for the AppBar and other primary color areas.
+  ),
+  home: Skoob(),
+)
 );
