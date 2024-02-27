7dd240dc1fcfa98932a68ed3012760be27a85d42
jiu keem <jeewoo0521@naver.com>
2024-02-27 14:02:55 +0900

    ui; apply right slide animation to BookDetail page

--- lib/app/views/widgets/bookshelf_detail_view_list_tile.dart
+++ lib/app/views/widgets/bookshelf_detail_view_list_tile.dart
@@ -24,7 +24,21 @@ class DetailViewListTile extends StatelessWidget with BookshelfListTileMixin {
               onTap: () {
                 Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => BookDetail(book: book))
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) => BookDetail(book: book),
+                      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.ease;
+
+                        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                          position: offsetAnimation,
+                          child: child,
+                        );
+                      })
                 );
               },
               child: Row(
