f4546a95df6f4e15c9916a4c68893a73a09f2067
jiu keem <jeewoo0521@naver.com>
2024-02-28 14:32:11 +0900

    ui; BookDetail page skeleton with NestedScrollView

--- lib/app/views/pages/book_detail.dart
+++ lib/app/views/pages/book_detail.dart
@@ -11,13 +11,83 @@ class BookDetail extends StatefulWidget {
   State<BookDetail> createState() => _BookDetailState();
 }
 
-class _BookDetailState extends State<BookDetail> {
+class _BookDetailState extends State<BookDetail> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('book detail page'),
-      ),
+      body: DefaultTabController(
+        length: 3,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                pinned: true,
+                floating: false,
+                expandedHeight: 200.0,
+                flexibleSpace: FlexibleSpaceBar(
+
+                ),
+              ),
+              SliverPersistentHeader(
+                  delegate: _SliverAppBarDelegate(
+                    TabBar(
+                      controller: _tabController,
+                      tabs: [
+                        Tab(text: 'INFO'),
+                        Tab(text: 'NOTE'),
+                        Tab(text: 'HIGHLIGHT'),
+                      ],
+                    ),
+                  ),
+                pinned: true,
+              ),
+            ];
+          },
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              Center(child: Text('INFO CONTENT'),),
+              Center(child: Text('NOTE CONTENT'),),
+              Center(child: Text('HIGHLIGHT CONTENT'),),
+            ],
+          ),
+        ),
+      )
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar _tabBar;
+
+  _SliverAppBarDelegate(this._tabBar);
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(child: _tabBar);
+  }
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
