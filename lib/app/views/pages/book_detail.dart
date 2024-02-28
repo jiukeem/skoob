7c86bf09fa76c7d5243e5f19f0a4704b2f7ac371
jiu keem <jeewoo0521@naver.com>
2024-02-28 16:56:01 +0900

    ui; trim tab style / set effect, selection color to white

--- lib/app/views/pages/book_detail.dart
+++ lib/app/views/pages/book_detail.dart
@@ -32,116 +32,142 @@ class _BookDetailState extends State<BookDetail> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     final Book book = widget.book;
     return Scaffold(
-      body: DefaultTabController(
-        length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                pinned: true,
-                floating: false,
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 120.0,
-                        height: 168.0,
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: AppColors.gray3,
-                            width: 0.5,
-                          )
-                        ),
-                        child: Image.network(
-                          book.basicInfo.coverImageUrl,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      SizedBox(height: 8.0),
-                      Text(
-                        book.basicInfo.title,
-                        style: TextStyle(
-                          fontFamily: 'NotoSansKRMedium',
-                          fontSize: 16.0,
-                          color: AppColors.softBlack
-                        ),
-                      ),
-                      SizedBox(height: 4.0),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: AppColors.white,
+        body: Theme(
+          data: ThemeData(
+            colorScheme: ColorScheme.fromSwatch().copyWith(
+              primary: AppColors.white
+            )
+          ),
+          child: DefaultTabController(
+            length: 3,
+            child: NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    backgroundColor: AppColors.white,
+                    surfaceTintColor: AppColors.white,
+                    pinned: true,
+                    floating: false,
+                  ),
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          Icon(
-                            FluentIcons.star_20_filled,
-                            color: AppColors.secondaryYellow,
-                            size: 32.0,
+                          Container(
+                            width: 120.0,
+                            height: 168.0,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: AppColors.gray3,
+                                width: 0.5,
+                              )
+                            ),
+                            child: Image.network(
+                              book.basicInfo.coverImageUrl,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                          Icon(
-                            FluentIcons.star_20_filled,
-                            color: AppColors.secondaryYellow,
-                            size: 32.0,
+                          SizedBox(height: 8.0),
+                          Text(
+                            book.basicInfo.title,
+                            style: TextStyle(
+                              fontFamily: 'NotoSansKRMedium',
+                              fontSize: 16.0,
+                              color: AppColors.softBlack
+                            ),
                           ),
-                          Icon(
-                            FluentIcons.star_20_filled,
-                            color: AppColors.secondaryYellow,
-                            size: 32.0,
+                          SizedBox(height: 4.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                FluentIcons.star_20_filled,
+                                color: AppColors.secondaryYellow,
+                                size: 32.0,
+                              ),
+                              Icon(
+                                FluentIcons.star_20_filled,
+                                color: AppColors.secondaryYellow,
+                                size: 32.0,
+                              ),
+                              Icon(
+                                FluentIcons.star_20_filled,
+                                color: AppColors.secondaryYellow,
+                                size: 32.0,
+                              ),
+                              Icon(
+                                FluentIcons.star_20_filled,
+                                color: AppColors.secondaryYellow,
+                                size: 32.0,
+                              ),
+                              Icon(
+                                FluentIcons.star_half_20_regular,
+                                color: AppColors.secondaryYellow,
+                                size: 32.0,
+                              ),
+                            ],
                           ),
-                          Icon(
-                            FluentIcons.star_20_filled,
-                            color: AppColors.secondaryYellow,
-                            size: 32.0,
+                          SizedBox(height: 8.0),
+                          Text(
+                            '국민의 모든 자유와 권리는 국가안전보장·질서유지 또는 공공복리를 위하여 필요한 경우에 한하여 법률로써 제한할 수 있으며, 제한하는 경우에도 자유와 권리의 본질적인 내용을 침해할 수 없다.',
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontFamily: 'NotoSansKrLight',
+                              fontSize: 12.0,
+                              color: AppColors.softBlack,
+                            ),
                           ),
-                          Icon(
-                            FluentIcons.star_half_20_regular,
-                            color: AppColors.secondaryYellow,
-                            size: 32.0,
-                          ),
+                          SizedBox(height: 20.0),
                         ],
                       ),
-                      SizedBox(height: 8.0),
-                      Text(
-                        '국민의 모든 자유와 권리는 국가안전보장·질서유지 또는 공공복리를 위하여 필요한 경우에 한하여 법률로써 제한할 수 있으며, 제한하는 경우에도 자유와 권리의 본질적인 내용을 침해할 수 없다.',
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontFamily: 'NotoSansKrLight',
-                          fontSize: 12.0,
-                          color: AppColors.softBlack,
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                    ],
-                  ),
-                ),
-              ),
-              SliverPersistentHeader(
-                  delegate: _SliverAppBarDelegate(
-                    TabBar(
-                      controller: _tabController,
-                      tabs: [
-                        Tab(text: 'INFO'),
-                        Tab(text: 'NOTE'),
-                        Tab(text: 'HIGHLIGHT'),
-                      ],
                     ),
                   ),
-                pinned: true,
+                  SliverPersistentHeader(
+                      delegate: _SliverAppBarDelegate(
+                        TabBar(
+                          controller: _tabController,
+                          tabs: const [
+                            Tab(text: 'INFO'),
+                            Tab(text: 'NOTE'),
+                            Tab(text: 'HIGHLIGHT'),
+                          ],
+                          labelStyle: const TextStyle(
+                            fontFamily: 'LexendSemiBold',
+                            fontSize: 16.0,
+                            color: AppColors.primaryGreen
+                          ),
+                          unselectedLabelStyle: const TextStyle(
+                              fontFamily: 'LexendRegular',
+                              fontSize: 16.0,
+                          ),
+                          unselectedLabelColor: AppColors.gray2,
+                          dividerColor: AppColors.gray2,
+                          indicatorColor: AppColors.primaryGreen,
+                          labelPadding: const EdgeInsets.symmetric(horizontal: 24.0),
+                          tabAlignment: TabAlignment.center,
+                          isScrollable: true,
+                          dividerHeight: 0.5,
+                        ),
+                      ),
+                    pinned: true,
+                  ),
+                ];
+              },
+              body: TabBarView(
+                controller: _tabController,
+                children: [
+                  Center(child: Text('INFO CONTENT'),),
+                  Center(child: Text('NOTE CONTENT'),),
+                  Center(child: Text('HIGHLIGHT CONTENT'),),
+                ],
               ),
-            ];
-          },
-          body: TabBarView(
-            controller: _tabController,
-            children: [
-              Center(child: Text('INFO CONTENT'),),
-              Center(child: Text('NOTE CONTENT'),),
-              Center(child: Text('HIGHLIGHT CONTENT'),),
-            ],
+            ),
           ),
         ),
-      )
     );
   }
 }
