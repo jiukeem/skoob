0bc7c659f7e625a6ba835b125207a74356ac56f7
jiu keem <jeewoo0521@naver.com>
2024-02-29 15:00:08 +0900

    feature; return different widget according to label type

--- lib/app/views/widgets/book_detail_info_list_view_tile.dart
+++ lib/app/views/widgets/book_detail_info_list_view_tile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:skoob/app/views/widgets/general_divider.dart';
 
 import '../../models/book.dart';
+import '../../models/book/custom_info.dart';
 import '../../utils/app_colors.dart';
 
 class BookDetailInfoListViewTile extends StatefulWidget {
@@ -15,24 +16,115 @@ class BookDetailInfoListViewTile extends StatefulWidget {
 }
 
 class _BookDetailInfoListViewTileState extends State<BookDetailInfoListViewTile> {
-  final List<String> labelList = [
+  final List<String>labels = [
     'Status',
-    'Started reading on',
     'Title',
     'Author',
     'Translator',
     'Publisher',
     'Publish Date',
     'Category',
-    'Link'
+    'Link',
   ];
+
+  Widget _generateTextWidget(String data) {
+    return Text(
+      data,
+      style: const TextStyle(
+        fontFamily: 'NotoSansKRRegular',
+        fontSize: 16.0,
+        color: AppColors.softBlack,
+      ),
+    );
+  }
+
+  Widget _generateStatusWidget(BookReadingStatus status) {
+    switch (status) {
+      case BookReadingStatus.initial:
+        return Text('initial');
+      case BookReadingStatus.notStarted:
+        return Container(
+          decoration: const BoxDecoration(
+            color: AppColors.gray2,
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 6.0),
+            child: Text(
+              'not started',
+              style: TextStyle(
+                  fontFamily: 'LexendRegular',
+                  color: AppColors.white,
+                  fontSize: 14.0),
+            ),
+          ),
+        );
+      case BookReadingStatus.reading:
+        return Container(
+          decoration: const BoxDecoration(
+            color: AppColors.secondaryYellow,
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 6.0),
+            child: Text(
+              'reading',
+              style: TextStyle(
+                  fontFamily: 'LexendRegular',
+                  color: AppColors.softBlack,
+                  fontSize: 14.0),
+            ),
+          ),
+        );
+      case BookReadingStatus.done:
+        return Container(
+          decoration: const BoxDecoration(
+            color: AppColors.softBlack,
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 6.0),
+            child: Text(
+              'done',
+              style: TextStyle(
+                  fontFamily: 'LexendRegular',
+                  color: AppColors.white,
+                  fontSize: 14.0),
+            ),
+          ),
+        );
+      default:
+        return Text('default');
+    }
+  }
+
+  Widget _generateContentWidget(String label) {
+    switch (label) {
+      case 'Status':
+        // final BookReadingStatus status = widget.book.customInfo.status;
+        return _generateStatusWidget(BookReadingStatus.reading);
+      case 'Title':
+        return _generateTextWidget(widget.book.basicInfo.title);
+      case 'Author':
+        return _generateTextWidget(widget.book.basicInfo.author);
+      case 'Translator':
+        return _generateTextWidget(widget.book.basicInfo.translator);
+      case 'Publisher':
+        return _generateTextWidget(widget.book.basicInfo.publisher);
+      case 'Publish Date':
+        return _generateTextWidget(widget.book.basicInfo.pubDate.toString().replaceAll('-', '.'));
+      case 'Category':
+        return _generateTextWidget(widget.book.basicInfo.category);
+      case 'Link':
+        return _generateTextWidget(widget.book.basicInfo.infoUrl);
+      default:
+        return _generateTextWidget('error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final String label = labelList[widget.index];
-    final String key = label.toLowerCase();
-    final Map<String, dynamic> valueMap = widget.book.basicInfo.toJson();
-
+    final label = labels[widget.index];
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -51,14 +143,7 @@ class _BookDetailInfoListViewTileState extends State<BookDetailInfoListViewTile>
                 ),
               ),
               const SizedBox(height: 4.0),
-              Text(
-                valueMap[key] ?? 'no result',
-                style: const TextStyle(
-                  fontFamily: 'NotoSansKRRegular',
-                  fontSize: 16.0,
-                  color: AppColors.softBlack,
-                ),
-              ),
+              _generateContentWidget(label),
             ],
           ),
         ),
