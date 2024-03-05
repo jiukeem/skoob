0e3a720bfd99f00f2718809888e1ca08e493353b
jiu keem <jeewoo0521@naver.com>
2024-03-05 15:39:11 +0900

    ui; implement textfield & bottom bar in UserRecord page

--- lib/app/views/pages/user_record.dart
+++ lib/app/views/pages/user_record.dart
@@ -1,4 +1,6 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:skoob/app/views/widgets/general_divider.dart';
 
 import '../../models/book.dart';
 import '../../utils/app_colors.dart';
@@ -19,6 +21,7 @@ class _UserRecordPageState extends State<UserRecord> {
 
     return Scaffold(
       backgroundColor: AppColors.white,
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         backgroundColor: AppColors.white,
         centerTitle: true,
@@ -44,7 +47,42 @@ class _UserRecordPageState extends State<UserRecord> {
           ],
         ),
       ),
-      body: Center(child: Text('user record'),),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
+              child: TextField(
+                maxLines: null,
+                expands: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                ),
+                cursorWidth: 1.2,
+                autofocus: true,
+                style: TextStyle(
+                  fontSize: 14.0
+                ),
+                keyboardType: TextInputType.multiline,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                GeneralDivider(padding: 0.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(FluentIcons.checkmark_16_filled))
+                  ],
+                ),
+              ],
+            )
+          )
+        ],
+      ),
     );
   }
 }
