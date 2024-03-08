c3344ea01f7f62b9cf8199e1ca3c0b4656cfd59a
jiu keem <jeewoo0521@naver.com>
2024-03-08 17:16:12 +0900

    feature; implement sorting in bookshelf

--- lib/app/views/pages/bookshelf.dart
+++ lib/app/views/pages/bookshelf.dart
@@ -10,8 +10,11 @@ import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:skoob/app/views/widgets/bookshelf_detail_view_list_tile.dart';
 import 'package:skoob/app/views/widgets/bookshelf_table_view_label.dart';
 
+import '../../models/book/custom_info.dart';
 import '../widgets/bookshelf_album_view_builder.dart';
 import '../widgets/bookshelf_table_view_list_tile.dart';
+import '../widgets/general_divider.dart';
+import '../widgets/sort_option_list_tile.dart';
 
 class Bookshelf extends StatefulWidget{
   const Bookshelf({super.key});
@@ -23,6 +26,8 @@ class Bookshelf extends StatefulWidget{
 class _BookshelfState extends State<Bookshelf> {
   BookshelfStatus _currentStatus = BookshelfStatus.loading;
   BookshelfViewOption _currentViewOption = BookshelfViewOption.detail;
+  SortOption _currentSortOption = SortOption.addedDate;
+  bool _isAscending = true;
 
   @override
   void initState() {
@@ -140,67 +145,73 @@ class _BookshelfState extends State<Bookshelf> {
                   padding: const EdgeInsets.fromLTRB(20.0, 4.0, 20.0, 20.0),
                   child: Row(
                     children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                              color: AppColors.gray1,
-                              width: 0.5,
-                            ),
-                            borderRadius: const BorderRadius.all(Radius.circular(20.0))
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
-                          child: Row(
-                            children: [
-                              Icon(
-                                FluentIcons.arrow_sort_16_regular,
+                      InkWell(
+                        onTap: () {
+                          _showSortOptionBottomSheet(context);
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
                                 color: AppColors.gray1,
-                                size: 14.0,
+                                width: 0.5,
                               ),
-                              SizedBox(width: 4.0,),
-                              Text(
-                                'sort',
-                                style: TextStyle(
-                                    color: AppColors.gray1,
-                                    fontFamily: 'LexendRegular',
-                                    fontSize: 14.0
+                              borderRadius: const BorderRadius.all(Radius.circular(20.0))
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  FluentIcons.arrow_sort_16_regular,
+                                  color: AppColors.gray1,
+                                  size: 14.0,
                                 ),
-                              )
-                            ],
+                                SizedBox(width: 4.0,),
+                                Text(
+                                  'sort',
+                                  style: TextStyle(
+                                      color: AppColors.gray1,
+                                      fontFamily: 'LexendRegular',
+                                      fontSize: 16.0
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       ),
                       const SizedBox(width: 12.0,),
-                      Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                              color: AppColors.gray1,
-                              width: 0.5,
-                            ),
-                            borderRadius: const BorderRadius.all(Radius.circular(20.0))
-                        ),
-                        child: const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
-                          child: Row(
-                            children: [
-                              Icon(
-                                FluentIcons.options_16_regular,
-                                color: AppColors.gray1,
-                                size: 14.0,
-                              ),
-                              SizedBox(width: 4.0,),
-                              Text(
-                                'filter',
-                                style: TextStyle(
-                                    color: AppColors.gray1,
-                                    fontFamily: 'LexendRegular',
-                                    fontSize: 14.0
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
+                      // TODO implement filter
+                      // Container(
+                      //   decoration: BoxDecoration(
+                      //       border: Border.all(
+                      //         color: AppColors.gray1,
+                      //         width: 0.5,
+                      //       ),
+                      //       borderRadius: const BorderRadius.all(Radius.circular(20.0))
+                      //   ),
+                      //   child: const Padding(
+                      //     padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
+                      //     child: Row(
+                      //       children: [
+                      //         Icon(
+                      //           FluentIcons.options_16_regular,
+                      //           color: AppColors.gray1,
+                      //           size: 14.0,
+                      //         ),
+                      //         SizedBox(width: 4.0,),
+                      //         Text(
+                      //           'filter',
+                      //           style: TextStyle(
+                      //               color: AppColors.gray1,
+                      //               fontFamily: 'LexendRegular',
+                      //               fontSize: 14.0
+                      //           ),
+                      //         )
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
                     ],
                   ),
                 ),
@@ -208,12 +219,13 @@ class _BookshelfState extends State<Bookshelf> {
                   const TableViewLabel(),
                 Expanded(
                   child: _currentViewOption == BookshelfViewOption.album
-                      ? BookshelfAlbumViewBuilder(items: listener.items)
+                      ? BookshelfAlbumViewBuilder(items: _sortBookshelf(listener.items))
                       : ListView.builder(
                           itemCount: listener.items.length,
                           itemBuilder: (context, index) {
-                            Book book = listener.items[index];
-                            bool isLast = index + 1 == listener.items.length;
+                            List<Book> sortedList = _sortBookshelf(listener.items);
+                            Book book = sortedList[index];
+                            bool isLast = index + 1 == sortedList.length;
                             if (_currentViewOption == BookshelfViewOption.detail) {
                               return DetailViewListTile(book: book, isLast: isLast);
                             } else if (_currentViewOption == BookshelfViewOption.table) {
@@ -230,7 +242,121 @@ class _BookshelfState extends State<Bookshelf> {
         }
     }
   }
+
+  List<Book> _sortBookshelf(List<Book> list) {
+    int statusComparator(BookReadingStatus a, BookReadingStatus b) {
+      const order = {
+        BookReadingStatus.initial: 0,
+        BookReadingStatus.notStarted: 1,
+        BookReadingStatus.reading: 2,
+        BookReadingStatus.done: 3,
+      };
+      return order[a]!.compareTo(order[b]!);
+    }
+
+    switch (_currentSortOption) {
+      case SortOption.title:
+        list.sort((a, b) => a.basicInfo.title.compareTo(b.basicInfo.title));
+        break;
+      case SortOption.rate:
+        list.sort((a, b) => a.customInfo.rate.compareTo(b.customInfo.rate));
+        break;
+      case SortOption.status:
+        list.sort((a, b) => statusComparator(a.customInfo.status, b.customInfo.status));
+        break;
+      case SortOption.startReadingDate:
+        list.sort((a, b) => a.customInfo.startReadingDate.compareTo(b.customInfo.startReadingDate));
+        break;
+      case SortOption.finishReadingDate:
+        list.sort((a, b) => a.customInfo.finishReadingDate.compareTo(b.customInfo.finishReadingDate));
+        break;
+      case SortOption.category:
+        list.sort((a, b) => a.basicInfo.category.compareTo(b.basicInfo.category));
+        break;
+      case SortOption.addedDate:
+        list.sort((a, b) => a.customInfo.addedDate.compareTo(b.customInfo.addedDate));
+        break;
+    }
+
+    if (!_isAscending) {
+      list = list.reversed.toList();
+    }
+
+    return list;
+  }
+
+  Future<void> _showSortOptionBottomSheet(BuildContext context) async {
+    final result = await showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+            decoration: const BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0)
+                )
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 12.0),
+                Container(
+                  width: 64,
+                  height: 2,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    color: AppColors.gray2,
+                  ),
+                ),
+                const SizedBox(height: 18.0),
+                const Text(
+                  'SORT',
+                  style: TextStyle(
+                      fontFamily: 'LexendRegular',
+                      fontSize: 16.0,
+                      color: AppColors.softBlack
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                const GeneralDivider(verticalPadding: 0),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: sortOptionMap.keys.length,
+                    itemBuilder: (context, index) {
+                      return SortOptionListTile(
+                          index: index,
+                          currentSortOption: _currentSortOption,
+                          isAscending: _isAscending
+                      );
+                    }),
+                ),
+              ],
+            ),
+          );
+        }
+    );
+
+    if (result != null) {
+      setState(() {
+         _currentSortOption = result['selectedOption'];
+         _isAscending = result['isAscending'];
+      });
+    }
+  }
 }
 
 enum BookshelfStatus { loading, complete }
 enum BookshelfViewOption { detail, table, album }
+enum SortOption { title, rate, status, startReadingDate, finishReadingDate, category, addedDate }
+
+final Map<String, SortOption> sortOptionMap = {
+  '제목순': SortOption.title,
+  '평점순': SortOption.rate,
+  '상태순': SortOption.status,
+  '시작한 날짜순': SortOption.startReadingDate,
+  '완독한 날짜순': SortOption.finishReadingDate,
+  '카테고리순': SortOption.category,
+  '추가한 날짜순': SortOption.addedDate
+};
+
---
+++ lib/app/views/widgets/sort_option_list_tile.dart
@@ -0,0 +1,56 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:skoob/app/views/widgets/general_divider.dart';
+
+import '../pages/bookshelf.dart';
+
+class SortOptionListTile extends StatefulWidget {
+  final int index;
+  final SortOption currentSortOption;
+  final bool isAscending;
+
+  const SortOptionListTile({super.key, required this.index,
+    required this.currentSortOption, required this.isAscending});
+
+  @override
+  State<SortOptionListTile> createState() => _SortOptionListTileState();
+}
+
+class _SortOptionListTileState extends State<SortOptionListTile> {
+  final Map<String, SortOption> labelMap = sortOptionMap;
+
+  @override
+  Widget build(BuildContext context) {
+    final label = sortOptionMap.keys.toList()[widget.index];
+    final isCurrentOption = labelMap[label] == widget.currentSortOption;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: InkWell(
+        onTap: () {
+          final bool isAscending =  isCurrentOption ? !widget.isAscending : true;
+          Navigator.pop(context, {'selectedOption': labelMap[label], 'isAscending': isAscending});
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(label),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12.0),
+                  child: isCurrentOption
+                      ? widget.isAscending ? const Icon(FluentIcons.chevron_up_24_regular) : const Icon(FluentIcons.chevron_down_24_regular)
+                      : const SizedBox.shrink(),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const GeneralDivider(verticalPadding: 0.0)
+          ],
+        ),
+      ),
+    );
+  }
+}
