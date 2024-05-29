941b698d93119095315b4a28ebaca77dcbfd8975
jiu keem <jeewoo0521@naver.com>
2024-05-29 17:19:26 +0900

    ui; add confirm dialog in logout feature

--- lib/app/views/pages/setting.dart
+++ lib/app/views/pages/setting.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:skoob/app/views/pages/auth_start.dart';
 
 import '../../controller/user_data_manager.dart';
+import '../../services/firebase_analytics.dart';
 import '../../utils/app_colors.dart';
-import 'intro_deprecated.dart';
 
 class Setting extends StatefulWidget {
   const Setting({super.key});
@@ -30,7 +30,7 @@ class _SettingState extends State<Setting> {
       ),
       body: InkWell(
         onTap: () {
-          _logout();
+          _showLogoutDialog(context);
         },
         child: const Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
@@ -56,5 +56,76 @@ class _SettingState extends State<Setting> {
       MaterialPageRoute(builder: (context) => const AuthStart()),
           (Route<dynamic> route) => false,
     );
+  }
+
+  Future<void> _showLogoutDialog(BuildContext context) async {
+    Widget cancelButton = InkWell(
+      child: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 16.0),
+        child: Text(
+          '취소',
+          style: TextStyle(
+            fontFamily: 'NotoSansKRMedium',
+            fontSize: 14.0,
+            color: AppColors.softBlack,
+          ),
+        ),
+      ),
+      onTap: () {
+        AnalyticsService.logEvent('Setting-- logout', parameters: {
+          'result': 'cancelled'
+        });
+        Navigator.of(context).pop(false);
+      },
+    );
+
+    Widget confirmButton = InkWell(
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          color: AppColors.primaryGreen,
+        ),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 16.0),
+          child: Text(
+            '로그아웃',
+            style: TextStyle(
+              fontFamily: 'NotoSansKRMedium',
+              fontSize: 14.0,
+              color: AppColors.white,
+            ),
+          ),
+        ),
+      ),
+      onTap: () {
+        AnalyticsService.logEvent('Setting-- logout', parameters: {
+          'result': 'logged out'
+        });
+        Navigator.of(context).pop(true);
+      },
+    );
+
+    AlertDialog alert = AlertDialog(
+      backgroundColor: AppColors.white,
+      surfaceTintColor: AppColors.white,
+      content: const Text('로그아웃 하시겠습니까?'),
+      actions: [
+        cancelButton,
+        confirmButton,
+      ],
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+      ),
+    );
+
+    final bool? shouldLogout = await showDialog(
+        context: context,
+        builder: (context) {
+          return alert;
+        });
+
+    if (mounted && shouldLogout == true) {
+      _logout();
+    }
   }
 }
